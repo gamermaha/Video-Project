ff234a886287aa88587efa0564c6fd13b99639ed
gamermaha <maha@revolvinggames.com>
2022-03-07 20:44:25 +0500

    main menu and main game function

---
+++ Assets/Monsters.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monsters : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [HideInInspector] 
+    public float speed;
+
+    private Rigidbody2D myBody;
+
+    private void Awake()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+        speed = 7;
+    }
+
+    void FixedUpdate()
+    {
+        myBody.velocity = new Vector2(speed, myBody.velocity.y);
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

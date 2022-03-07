f5a5c408a5f60838ed009b5dc15a2b769a236eec
gamermaha <maha@revolvinggames.com>
2022-03-07 21:19:11 +0500

    main menu and main game function at 6:20

---
+++ Assets/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] 
+    private GameObject[] players;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/MainMenuController.cs
+++ Assets/MainMenuController.cs
@@ -1,18 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void playGame()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        int selectedCharacter =
+            int.Parse(UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name);
+        //SceneManager.LoadScene("Gameplay");
     }
 }
